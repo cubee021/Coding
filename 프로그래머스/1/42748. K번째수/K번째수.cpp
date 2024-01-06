d26c59d5a57ea6478f967217bdb5e5b037c6a40e
cube021 <98734049+cube021@users.noreply.github.com>
2024-01-06 22:18:16 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -7,12 +7,9 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     for(auto& c : commands)
     {
-        vector<int> v;
-        for(int i=c[0]-1; i<c[1]; i++)
-            v.push_back(array[i]);
-        
-        sort(v.begin(), v.end());
-        answer.push_back(v[c[2]-1]);
+        vector<int> temp = array;
+        sort(temp.begin()+c[0]-1, temp.begin()+c[1]);
+        answer.push_back(temp[c[0]+c[2]-2]);
     }
     
     return answer;
