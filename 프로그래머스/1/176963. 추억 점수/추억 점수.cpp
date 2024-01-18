cc4f0ba011b38089897152f2dde27739ea0acbeb
cube021 <98734049+cube021@users.noreply.github.com>
2024-01-19 00:18:13 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    map<string, int> map;
+    
+    for(int i=0; i<name.size(); i++)
+        map.insert({name[i], yearning[i]});
+    
+    for(auto& p : photo)
+    {
+        int temp = 0;
+        for(string s : p)
+        {
+            if(map.find(s) != map.end())
+            {
+                auto target = map.find(s);
+                temp += target->second;
+            }
+        }
+        answer.push_back(temp);
+    }
+    
+    return answer;
+}
