1777a53868d1c0ae00018463f56ea5c6679180c0
cube021 <98734049+cube021@users.noreply.github.com>
2024-01-20 21:39:27 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.06 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -7,9 +7,12 @@ int solution(int number, int limit, int power) {
     
     for(int i=1; i<=number; i++)
     {
-        int count = 1;
-        for(int j=1; j<=i/2; j++)
-            if(i%j == 0) count++;
+        int count = 0;
+        for(int j=1; j*j<=i; j++)
+        {
+            if(j*j == i) count++;
+            else if(i%j == 0) count += 2;
+        }
         
         answer += count > limit ? power : count;
     }
