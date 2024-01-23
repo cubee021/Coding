f1901c478c46b75daeaca92bb4235bb4d184c95c
cube021 <98734049+cube021@users.noreply.github.com>
2024-01-23 19:05:23 +0900

    [level 1] Title: 폰켓몬, Time: 0.17 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,25 +1,10 @@
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    vector<int> temp{nums[0]};
-    int answer = 0;
+    unordered_set<int> set(nums.begin(), nums.end());
     
-    for(int i=1; i<nums.size(); i++)
-    {
-        int j;
-        for(j=0; j<temp.size(); j++)
-        {
-            if(temp[j] == nums[i])
-                break;
-        }
-        if(j == temp.size())
-            temp.push_back(nums[i]);
-        
-    }
-        
-    answer = temp.size() > nums.size()/2 ? nums.size()/2 : temp.size();
-    
-    return answer;
+    return set.size() > nums.size()/2 ? nums.size()/2 : set.size();
 }
