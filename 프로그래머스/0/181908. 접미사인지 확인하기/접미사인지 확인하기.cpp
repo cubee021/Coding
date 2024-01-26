ed6161b2d798761528ba7a3647cb9f1ddea5123d
cube021 <98734049+cube021@users.noreply.github.com>
2024-01-26 23:36:05 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 int solution(string my_string, string is_suffix) {
-    set<string> s;
-    for(int i=0; i<my_string.length(); i++)
-        s.insert(my_string.substr(i, my_string.length()-i));
     
-    if(s.find(is_suffix) != s.end())
-        return 1;
+    for(int i=0; i<my_string.length(); i++)
+    {
+        string temp = my_string.substr(i);
+        
+        if(temp == is_suffix)
+            return 1;
+    }
     
     return 0;
     
