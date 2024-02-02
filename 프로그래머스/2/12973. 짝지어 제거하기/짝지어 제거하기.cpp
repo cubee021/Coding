ede50736325fd32a6bf90f68a7433dfef5f78165
cube021 <98734049+cube021@users.noreply.github.com>
2024-02-02 22:02:20 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.50 ms, Memory: 6.43 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> stack;
+    for(char c : s)
+    {
+        if(!stack.empty() && stack.top() == c)
+            stack.pop();
+        else
+            stack.push(c);
+    }
+    
+    return stack.empty() ? 1 : 0;
+}
