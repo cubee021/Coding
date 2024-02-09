436158554560b4c8ed6ed6ca80ea74f2887577bb
cube021 <98734049+cube021@users.noreply.github.com>
2024-02-09 21:34:30 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -4,16 +4,8 @@
 using namespace std;
 
 string solution(string my_string, string letter) {
-    string answer = "";
     
-    for(char c : my_string)
-    {
-        string temp = "";
-        temp += c;
-        
-        if(temp != letter)
-            answer += c;
-    }
+    my_string.erase(remove(my_string.begin(), my_string.end(), letter[0]), my_string.end());
     
-    return answer;
+    return my_string;
 }
