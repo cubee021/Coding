4d2af0871ec10bad5f8d725f654efde2d0958117
cube021 <98734049+cube021@users.noreply.github.com>
2024-02-15 13:37:57 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3698.39 ms, Memory: 23.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,12 +1,10 @@
 #include <string>
 #include <vector>
-#include <deque>
 #include <set>
 using namespace std;
 
 int solution(vector<int> elements) {
     set<int> answers;
-    deque<int> dq(elements.begin(), elements.end());
     
     for(int i=1; i<=elements.size(); i++) // 추출할 길이
     {
@@ -15,22 +13,11 @@ int solution(vector<int> elements) {
         for(int j=0; j<elements.size(); j++)
         {
             for(int k=0; k<i; k++)
-            {
-                sum += dq.front();
-                dq.push_back(dq.front());
-                dq.pop_front();
-            }
+                sum += elements[(j+k)%elements.size()];
             
             answers.insert(sum);
             sum = 0;
-            
-            for(int k=0; k<i-1; k++)
-            {
-                dq.push_front(dq.back());
-                dq.pop_back();
-            }
         }
-        
     }
     
     return answers.size();
