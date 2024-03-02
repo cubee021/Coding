a2d9f51deda8c274aff505be9fa5fbd6dd337b69
cube021 <98734049+cube021@users.noreply.github.com>
2024-03-03 01:02:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(int& n)
+{
+	vector<int> DP = { 1, 2 };
+
+	if (n > 1)
+	{
+		for (int i = 2; i < n; i++)
+		{
+			DP.push_back((DP[i - 1] + DP[i - 2])%10007);
+		}
+	}
+
+	return DP[n - 1];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << solution(n);
+}
