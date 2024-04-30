9ab4669278a44a6b3818bfc2b2de5ddd03a6fecf
cube021 <98734049+cube021@users.noreply.github.com>
2024-05-01 00:09:01 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -4,25 +4,14 @@ using namespace std;
 
 int solution(int n, int a, int b)
 {
-    int turn = 1;
+    int answer = 0;
     a -= 1, b -= 1;
     
-    while(turn < n)
+    while(a != b)
     {   
-        if(a < b)
-        {
-            if(a+1 == b && a%2 == 0 && b%2 == 1)
-                return turn;
-        }
-        else
-        {
-            if(a == b+1 && b%2 == 0 && a%2 == 1)
-                return turn;
-        }
-        
-        turn++;
+        answer++;
         a /= 2, b /= 2;
     }
     
-    return -1;
+    return answer;
 }
