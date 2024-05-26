8e62e3f80922b7a9a2a5b5fc874651377f339688
cube021 <98734049+cube021@users.noreply.github.com>
2024-05-27 00:11:29 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<bool> used(10, false);
+vector<int> answer(10, 0);
+
+void sequence(vector<int>& num, int N, int M, int prev = 0, int idx = 0)
+{
+	if (idx == M)
+	{
+		for (int i = 0; i < idx; i++) cout << answer[i] << " ";
+		cout << '\n';
+		return;
+	}
+
+	for (int i=0; i<num.size(); i++)
+	{
+		if (used[i]) continue;
+		if (num[prev] > num[i]) continue;
+
+		answer[idx] = num[i];
+		used[i] = true;
+		sequence(num, N, M, i,idx + 1);
+		used[i] = false;
+	}
+}
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> num;
+	for (int i = 0; i < N; i++)
+	{
+		int k;
+		cin >> k;
+		num.push_back(k);
+	}
+
+	sort(num.begin(), num.end());
+
+	sequence(num ,N, M);
+}
