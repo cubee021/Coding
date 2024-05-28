b05d6e67420b14c4dcebecac5c4048c529641760
cube021 <98734049+cube021@users.noreply.github.com>
2024-05-28 20:51:31 +0900

    [Silver III] Title: 다음 순열, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.cc
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -3,27 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-bool next_permutation_(vector<int>& v, int N)
-{
-	int i = N - 1;
-	while (i > 0 && v[i - 1] >= v[i]) i--;
-
-	if (i <= 0) return false;
-
-	int j = N - 1;
-	while (v[j] <= v[i - 1]) j--;
-	swap(v[j], v[i - 1]);
-
-	j = N - 1;
-	while (i < j)
-	{
-		swap(v[i], v[j]);
-		i++, j--;
-	}
-
-	return true;
-}
-
 int main()
 {
 	int N;
@@ -38,7 +17,7 @@ int main()
 		v.push_back(a);
 	}
 
-	if (!next_permutation_(v, N)) cout << -1;
+	if (!next_permutation(v.begin(), v.end())) cout << -1;
 	else
 	{
 		for (int i : v) cout << i << " ";
