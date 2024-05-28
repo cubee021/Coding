37737838dfd22229c4dee154e9c6f23f042c7f1c
cube021 <98734049+cube021@users.noreply.github.com>
2024-05-28 20:59:49 +0900

    [Silver III] Title: 이전 순열, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> v;
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+
+		v.push_back(a);
+	}
+
+	if (!prev_permutation(v.begin(), v.end())) cout << -1;
+	else
+	{
+		for (int i : v) cout << i << " ";
+	}
+}
