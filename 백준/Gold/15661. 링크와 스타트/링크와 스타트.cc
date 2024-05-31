90bf345aed96f27480f8e418687f54df41123aa2
cube021 <98734049+cube021@users.noreply.github.com>
2024-05-31 21:36:03 +0900

    [Gold V] Title: 링크와 스타트, Time: 360 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int answer = 2147483647;
+vector<int> team1;
+vector<int> team2;
+
+void get_min(vector<vector<int>>& power, vector<int> team1, vector<int> team2, int idx = 0)
+{
+	int n = power.size();
+
+	if (idx == n)
+	{
+		if (team1.size() < 1 || team2.size() < 1) return;
+
+		int sum1 = 0;
+		int sum2 = 0;
+
+		for (int i = 0; i < team1.size()-1; i++)
+			for (int j = i+1; j < team1.size(); j++)
+				if(i != j) sum1 += power[team1[i]][team1[j]] + power[team1[j]][team1[i]];
+
+		for (int i = 0; i < team2.size()-1; i++)
+			for (int j = i+1; j < team2.size(); j++)
+				if (i != j) sum2 += power[team2[i]][team2[j]] + power[team2[j]][team2[i]];
+
+		answer = min(answer, abs(sum1 - sum2));
+		
+		return;
+	}
+
+	team1.push_back(idx);
+	get_min(power, team1, team2, idx + 1);
+	team1.pop_back();
+
+	team2.push_back(idx);
+	get_min(power, team1, team2, idx + 1);
+	team2.pop_back();
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<vector<int>> power(N, vector<int>(N, 0));
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			int a;
+			cin >> a;
+
+			power[i][j] = a;
+		}
+	}
+
+	get_min(power, team1, team2);
+
+	cout << answer;
+}
