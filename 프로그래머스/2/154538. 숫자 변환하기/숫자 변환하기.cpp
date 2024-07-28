cb4b5e02855563c6274001c9a7abee39fc2b6d26
cubee021 <98734049+cubee021@users.noreply.github.com>
2024-07-28 23:46:41 +0900

    [level 2] Title: 숫자 변환하기, Time: 3.75 ms, Memory: 5 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -4,37 +4,26 @@
 using namespace std;
 
 int solution(int x, int y, int n) {
-    vector<int> dist(3000001, -1);
-    queue<int> q;
-    
-    q.push(x);
-    dist[x] = 0;
+    queue<pair<int, int>> q;
+    q.push({y, 0});
     
     while(!q.empty())
     {
-        int cur = q.front();
+        int cur = q.front().first;
+        int cnt = q.front().second;
         q.pop();
         
-        if(cur == y) return dist[cur];
-        else if(cur > y) continue;
+        if(cur == x) return cnt;
         
-        if(dist[cur+n] == -1)
-        {
-            q.push(cur + n);
-            dist[cur+n] = dist[cur] + 1;
-        }
+        if(cur - n >= 0)
+            q.push({cur-n, cnt+1});
         
-        if(dist[cur*2] == -1)
-        {
-            q.push(cur * 2);
-            dist[cur*2] = dist[cur] + 1;
-        }
+        if(cur%2 == 0)
+            q.push({cur/2, cnt+1});
         
-        if(dist[cur*3] == -1)
-        {
-            q.push(cur * 3);
-            dist[cur*3] = dist[cur] + 1;
-        }
+        if(cur%3 == 0)
+            q.push({cur/3, cnt+1});
+        
     }
     
     return -1;
