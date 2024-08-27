edf1f4e6a5ea8ab2d30463dca93e72611e7ac6f5
cubee021 <98734049+cubee021@users.noreply.github.com>
2024-08-27 14:03:03 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        int num = i;
+        int sum = i;
+
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if (sum == N)
+        {
+            cout << i;
+            return 0;
+        }
+    }
+
+    cout << 0;
+}
