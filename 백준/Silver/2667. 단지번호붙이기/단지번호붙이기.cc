14449b0c00151d0724c342493b3b75557c6ed857
cubee021 <98734049+cubee021@users.noreply.github.com>
2025-08-07 10:01:18 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -4,74 +4,64 @@
 #include <queue>
 using namespace std;
 
+int N;
+vector<pair<int, int>> dh = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+
 vector<int> answer;
 
-void BFS(vector<string>& board, vector<vector<bool>>& check, pair<int, int> cur)
+void BFS(vector<vector<int>>& board, int i, int j)
 {
-	int n = board.size();
-	vector<pair<int, int>> dh = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-
 	queue<pair<int, int>> q;
-	q.push({ cur.first, cur.second });
-	check[cur.first][cur.second] = true;
+	q.push({ i, j });
+	board[i][j] = 0;
 
-	int cnt = 0;
+	int count = 0;
+
 	while (!q.empty())
 	{
-		int i = q.front().first;
-		int j = q.front().second;
-		cnt++;
-
+		int x = q.front().first;
+		int y = q.front().second;
 		q.pop();
 
-		for (auto h : dh)
-		{
-			int x = i + h.first;
-			int y = j + h.second;
+		count++;
 
-			if (x >= 0 && x < n && y >= 0 && y < n)
+		for (pair<int, int> h : dh)
+		{
+			int nx = x + h.first;
+			int ny = y + h.second;
+
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (board[nx][ny] == 1)
 			{
-				if (!check[x][y] && board[x][y] == '1')
-				{
-					q.push({ x, y });
-					check[x][y] = true;
-				}
+				q.push({ nx, ny });
+				board[nx][ny] = 0;
 			}
 		}
 	}
 
-	answer.push_back(cnt);
+	answer.push_back(count);
 }
 
 int main()
 {
-	int N;
 	cin >> N;
 
-	vector<string> board;
-	vector<vector<bool>> check(N, vector<bool>(N, false));
+	vector<vector<int>> board(N, vector<int>(N));
 	for (int i = 0; i < N; i++)
-	{
-		string s;
-		cin >> s;
-
-		board.push_back(s);
-	}
+		for (int j = 0; j < N; j++)
+			scanf("%1d", &board[i][j]);
 
 	for (int i = 0; i < N; i++)
-	{
 		for (int j = 0; j < N; j++)
 		{
-			if (!check[i][j] && board[i][j] == '1')
-				BFS(board, check, {i, j});
+			if (board[i][j] == 1)
+				BFS(board, i, j);
 		}
-	}
-	
-	cout << answer.size() << '\n';
-	sort(answer.begin(), answer.end());
-	for (int i : answer)
-	{
-		cout << i << '\n';
-	}
 
+	sort(answer.begin(), answer.end());
+
+	cout << answer.size() <<'\n';
+
+	for (int x : answer)
+		cout << x << '\n';
 }
